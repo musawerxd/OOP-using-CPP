6f1177a387ee1b374bc9e40531878d155c8ac6ff
Musawer Ahmad <hunnyshah0012@gmail.com>
2026-01-18 22:31:27 +0500

    3.11 done

---
+++ Ch#3-Programming in cpp/Example-Questions/3.11.cpp
@@ -0,0 +1,14 @@
+// Write a program that displays the sizes of different data types.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Size of 'char' Data Type in Bytes: " << sizeof(char) << endl;
+	cout << "Size of 'int' Data Type in Bytes: " << sizeof(int) << endl;
+	cout << "Size of 'float' Data Type in Bytes: " << sizeof(float) << endl;
+	cout << "Size of 'long' Data Type in Bytes: " << sizeof(long) << endl;
+	cout << "Size of 'double' Data Type in Bytes: " << sizeof(double) << endl;
+	cout << "Size of 'long double' Data Type in Bytes: " << sizeof(long double) << endl;
+	return 0;
+}
