520166139e6a429311bfa3269ec5e6adeeb42079
Musawer Ahmad <hunnyshah0012@gmail.com>
2026-01-18 22:21:21 +0500

    3.4 done

--- Ch#3-Programming in cpp/Example-Questions/3.4.cpp
+++ Ch#3-Programming in cpp/Example-Questions/3.4.cpp
@@ -1,20 +1,21 @@
-//Write a program that performs all compound assignment operations on an integer.
+// Write a program that performs all compound assignment operations on an integer.
 
 #include <iostream>
 using namespace std;
 int main()
 {
-		int a=10;
-		cout<<"Value of a: "<<a<<endl<<endl;
-		a+=5;
-		cout<<"Value of a after a+=5: "<<a<<endl;
-		a-=5;
-		cout<<"Value of a after a-=5: "<<a<<endl;
-		a*=2;
-		cout<<"Value of a after a*=2: "<<a<<endl;
-		a/=2;
-		cout<<"Value of a after a/=2: "<<a<<endl;
-		a%=2;
-		cout<<"Value of a after a%=2: "<<a;
-		return 0;
+	int a = 10;
+	cout << "Value of a: " << a << endl
+		 << endl;
+	a += 5;
+	cout << "Value of a after a+=5: " << a << endl;
+	a -= 5;
+	cout << "Value of a after a-=5: " << a << endl;
+	a *= 2;
+	cout << "Value of a after a*=2: " << a << endl;
+	a /= 2;
+	cout << "Value of a after a/=2: " << a << endl;
+	a %= 2;
+	cout << "Value of a after a%=2: " << a;
+	return 0;
 }
