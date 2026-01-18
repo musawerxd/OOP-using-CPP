99b4a5600e6d40d132ca23e1c57717fc4552d090
Musawer Ahmad <hunnyshah0012@gmail.com>
2026-01-18 22:24:46 +0500

    3.7 done

---
+++ Ch#3-Programming in cpp/Example-Questions/3.7.cpp
@@ -0,0 +1,16 @@
+//Write a program that explains the difference between postfix decrement operator and prefix decrement operator used as an independent expression.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+		int a,b,x,y;
+		a=b=x=y=0;
+		a--;
+		b=a;
+		--x;
+		y=x;
+		cout<<"a: "<<a<<endl<<"b: "<<b<<endl;
+		cout<<"x: "<<x<<endl<<"y: "<<y<<endl;
+		return 0;
+}
