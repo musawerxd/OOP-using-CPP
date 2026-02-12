c28d6d8b1b015c01949a2f3c932add21aede4e48
musawerxd <hunnyshah0012@gmail.com>
2026-02-12 18:51:35 +0500

    ch#6 Example 6.1--6.3 Done

---
+++ Ch#6-Loops/Example-Questions/6.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 1, sum = 0;
+	while (n <= 5)
+	{
+		cout << n << endl;
+		sum = sum + n; 
+		n = n + 1;
+	}
+	cout << "Sum is: " << sum;
+	return 0;
+}
