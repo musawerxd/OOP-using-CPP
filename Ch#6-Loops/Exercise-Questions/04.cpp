3d1caad122cb6430dbe56d297b76f497a7fc05f6
musawerxd <hunnyshah0012@gmail.com>
2026-02-26 20:56:23 +0500

    add Ex Q4 from chapter 6

---
+++ Ch#6-Loops/Exercise-Questions/04.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Alphabets from 'A' to 'Z' are as follows:\n\n";
+	for (char ch = 'A'; ch <= 'Z'; ch++)
+		cout << ch << "\t";
+	return 0;
+}
