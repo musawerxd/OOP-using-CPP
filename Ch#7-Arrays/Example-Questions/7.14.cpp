da3e039a608bcd7952d57674923ee4e71f98f715
musawerxd <hunnyshah0012@gmail.com>
2026-04-05 14:01:27 +0500

    solve chapter 7 Example 7.14 --7.15

---
+++ Ch#7-Arrays/Example-Questions/7.14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int arr[2][4], i, j;
+	for (i = 0; i < 2; i++)
+		for (j = 0; j < 4; j++)
+		{
+			cout << "Enter a Value: ";
+			cin >> arr[i][j];
+		}
+	cout << endl;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+			cout << arr[i][j] << "\t";
+		cout << endl;
+	}
+	return 0;
+}
