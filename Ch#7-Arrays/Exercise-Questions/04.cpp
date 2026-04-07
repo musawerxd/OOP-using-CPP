eac0896e54c150903102da895aaf7a5fb6f7533b
musawerxd <hunnyshah0012@gmail.com>
2026-04-07 20:47:44 +0500

    solve chapter 7 Exercise Q4

---
+++ Ch#7-Arrays/Exercise-Questions/04.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	char name[10][30];
+	int sal[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cin.clear();
+		cin.sync();
+		cout << "Enter the Name of an Employee: ";
+		gets(name[i]);
+		cout << "Enter the Salary: ";
+		cin >> sal[i];
+	}
+	cout << setw(30) << left << "\nName" << "Salary" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		if (sal[i] * 12 >= 250000)
+			cout << setw(30) << name[i] << setw(10) << sal[i] << "\tTax to be Paid!" << endl;
+		else
+			cout << setw(30) << name[i] << setw(10) << sal[i] << "\tNo Tax" << endl;
+	}
+	return 0;
+}
